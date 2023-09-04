59737411a831fbb4d07e24e60e4175985f0ce161
elliotalders <142671065+elliotalders@users.noreply.github.com>
2023-09-04 02:45:58 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+def my_name():
+     print("my name is luffy")
+
+my_name() 
+
+
+def my_meals(food, drink):
+     print(f"i like to eat {food} and while drinking {drink}")
+my_meals("kfc", "pepsi")
+
+
+
+def cube(number):
+    return number**3
+print(cube(3))
+
+
+def by_three(number):
+     if number %3 == 0:
+          return cube(number)
+     else :
+          return "false"
+print (by_three(2))
+          
+
+    
+    
+     
